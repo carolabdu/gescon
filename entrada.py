b4fc85a48ff6c0c697ae7cb8d376a56a20adf7b7
carolabdu <108949383+carolabdu@users.noreply.github.com>
2025-06-24 22:49:10 -0300

    Delete entrada.py

--- entrada.py
+++
@@ -1,7 +0,0 @@
-class Entrada: 
-
-  def __init__(self, dados, max_min):
-    self.dados_normalizados = []
-    for i in range len(dados): 
-      x = (dados[i] - max_min[i][0])/(max_min[i][1] -max_min[i][0])
-      self.dados_normalizados.append(x)
